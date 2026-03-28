b3d4566cf36299ae4e67f7b6b218f3ba19fa6dc0
Priyanka Mishra <prmishra3085@gmail.com>
2026-03-28 18:37:13 +0530

    Update maxDepth.cpp

--- BinaryTree/maxDepth.cpp
+++ BinaryTree/maxDepth.cpp
@@ -1,3 +1,29 @@
 //Problem Explanation:Maximum depth of binary tree ==finding the total number of levels in binary tree.
 //Logic:count the levels of binary tree
 //Algorithum:Use level order traversal(BFS)
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==NULL)
+        return 0;
+        queue<TreeNode*>q;
+        q.push(root);
+        int maxDep=0;
+        while(!q.empty())
+        {
+            int size=q.size();
+            while(size--)
+            {
+                TreeNode*temp=q.front();
+                q.pop();
+                if(temp->left!=NULL)
+                q.push(temp->left);
+                if(temp->right!=NULL)
+                q.push(temp->right);
+            }
+            maxDep++;
+        }
+        return maxDep;
+        
+    }
+};
