4845335ee55ce22808a2a57d963d4bcd73a2f0bc
Priyanka Mishra <prmishra3085@gmail.com>
2026-04-02 21:31:12 +0530

    Update DijkstraAlgo.cpp

--- Graphs/DijkstraAlgo.cpp
+++ Graphs/DijkstraAlgo.cpp
@@ -51,3 +51,51 @@ class Solution {
     }
 };
 //Method2:(optimized)
+//using min heap
+class Solution {
+  public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+          // Convert edges into adj list(Graph)
+        vector<vector<pair<int,int>>>adj(V);
+        for(int i=0;i<edges.size();i++)
+        {
+            int u=edges[i][0];
+            int v=edges[i][1];
+            int w=edges[i][2];
+            adj[u].push_back(make_pair(v,w));
+            adj[v].push_back(make_pair(u,w));
+        }
+        //take a visited array
+        vector<bool>visited(V,0);
+        //take a dist array (min dist of all nodes from src)
+        vector<int>dist(V,INT_MAX);
+        dist[src]=0;
+        //for selection min dist node use min heap
+        priority_queue<pair<int,int>,vector< pair<int,int>>,greater< pair<int,int>>>p;
+        p.push(make_pair(0,src));
+        while(!p.empty())
+        {
+            //find min dist node
+            int node=p.top().second;
+            p.pop();
+            //check node is visited or not
+            if(visited[node]==1)
+            continue;
+            //not visited
+            visited[node]=1;
+            //check for neig node of node
+            for(int i=0;i<adj[node].size();i++)
+            {
+                int neig=adj[node][i].first;
+                int weight=adj[node][i].second;
+                if(visited[neig]==0&&dist[neig]>dist[node]+weight)
+                {
+                   dist[neig]=dist[node]+weight;
+                   p.push(make_pair(dist[neig],neig));
+                }
+            }
+        }
+        return dist;
+        
+    }
+};
