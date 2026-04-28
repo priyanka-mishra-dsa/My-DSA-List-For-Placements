8dd6d34698c0f98860a25ca984d3bd10d384a3c2
Priyanka Mishra <prmishra3085@gmail.com>
2026-04-28 21:22:29 +0530

    Update eularPath.cpp

--- Graphs/eularPath.cpp
+++ Graphs/eularPath.cpp
@@ -1,1 +1,4 @@
 //Eular Path:it is a path in a graph which visits all the edgest of graphs exactly one time
+//each graph does not contain eular path.
+//Eular Circuit:a circuit which contains eular path and have a circle (start and end node should be same)
+
