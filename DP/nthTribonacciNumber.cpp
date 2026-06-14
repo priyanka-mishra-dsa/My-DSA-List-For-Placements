ad5bcaff40f1ec80b3a8798762a4347fcdca5396
Priyanka Mishra <prmishra3085@gmail.com>
2026-06-14 21:15:55 +0530

    Update nthTribonacciNumber.cpp

--- DP/nthTribonacciNumber.cpp
+++ DP/nthTribonacciNumber.cpp
@@ -23,3 +23,31 @@ int tribonacci(int n) {
         return ans;   
     }
 };
+//Using Tabulation(Bottom-up method)
+class Solution {
+public:
+int solveUsingTabul(int n)
+{
+    vector<int>dp(n+1,-1);
+    //base cases 
+    dp[0]=0;
+    dp[1]=1;
+    dp[2]=1;
+    for(int i=3;i<=n;i++)
+    {
+    int res1=dp[i-1];
+    int res2=dp[i-2];
+    int res3=dp[i-3];
+    int sum=res1+res2+res3;
+    //store that value
+    dp[i]=sum;
+    }
+    return dp[n];
+}
+    int tribonacci(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+        int ans=solveUsingTabul(n);
+        return ans;   
+    }
+};
