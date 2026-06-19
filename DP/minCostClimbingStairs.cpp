0ca3e07b672574a231d20d940e2d94acd4fd5fc4
Priyanka Mishra <prmishra3085@gmail.com>
2026-06-19 14:45:11 +0530

    Update minCostClimbingStairs.cpp

--- DP/minCostClimbingStairs.cpp
+++ DP/minCostClimbingStairs.cpp
@@ -24,3 +24,33 @@ int recursion(vector<int>&cost,int index)
     }
 };
 //Using memorization Dp
+class Solution {
+public:
+int Memorization(vector<int>&cost,vector<int>&dp,int index)
+{
+    
+    int n=cost.size();
+    //base case
+    if(index>=n)
+    return 0;
+    if(dp[index]!=-1)
+    return dp[index];
+    int climb1step=Memorization(cost,dp,index+1)+cost[index];
+    int climb2step=Memorization(cost,dp,index+2)+cost[index];
+    int mincost=min(climb1step,climb2step);
+    //store the value
+    dp[index]=mincost;
+    return mincost;
+}
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n=cost.size();
+        vector<int>dp(n+1,-1);
+        //start from index 0
+        int cost1=Memorization(cost,dp,0);
+        //start from index 1
+        int cost2=Memorization(cost,dp,1);
+        //return min cost among them
+        int minCost=min(cost1,cost2);
+        return minCost; 
+    }
+};
