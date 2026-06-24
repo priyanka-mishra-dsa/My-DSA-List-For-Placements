7c58dd5464f1948820d92c8a5e6bee879b55723d
Priyanka Mishra <prmishra3085@gmail.com>
2026-06-24 16:24:56 +0530

    Update Majority Element.cpp

---
+++ hashMap/Majority Element.cpp
@@ -0,0 +1,23 @@
+//Using unordered map for count the freq
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>m;
+        //count freq of each element
+        for(int i=0;i<n;i++)
+        {
+            m[nums[i]]++;
+        }
+        //check in map for n/2 freq wala element
+        for(auto it:m)
+        {
+            if(it.second>n/2)
+            {
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
+//Boyer Moore voting algorithum
