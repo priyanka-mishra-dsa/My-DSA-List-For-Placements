de5ae24b755c845150ce04bd7cd2d3e3f52b4bb4
Priyanka Mishra <prmishra3085@gmail.com>
2026-07-07 15:01:54 +0530

    Update SolvingquestionswithBrainPower.cpp

--- DP/SolvingquestionswithBrainPower.cpp
+++ DP/SolvingquestionswithBrainPower.cpp
@@ -22,4 +22,27 @@ long long solve(int index,vector<vector<int>>& questions,int n,vector<long long>
         vector<long long>dp(n+1,-1);
         long long ans=solve(0,questions,n,dp);
         return ans;    
-    
+//Tabulation
+class Solution {
+public:
+    long long mostPoints(vector<vector<int>>& ques) {
+        int n=ques.size();
+        vector<long long>dp(n+1,0);
+        //Base case
+        dp[n]=0;
+        for(int index=n-1;index>=0;index--)
+        {
+            long long solveQue=ques[index][0];
+            long long nextQueIndex=index+ques[index][1]+1;
+            if(nextQueIndex<n)
+            {
+                solveQue=solveQue+dp[nextQueIndex];
+            }
+            long long skipQue=dp[index+1];
+            long long maxPoint=max(solveQue,skipQue);
+            //store
+            dp[index]=maxPoint;
+        }
+        return dp[0];    
+    }
+};
