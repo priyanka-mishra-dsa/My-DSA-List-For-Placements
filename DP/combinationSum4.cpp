ba443b5401853d2b9d53da41832680ef42fe31e3
Priyanka Mishra <prmishra3085@gmail.com>
2026-07-08 22:11:10 +0530

    Update combinationSum4.cpp

--- DP/combinationSum4.cpp
+++ DP/combinationSum4.cpp
@@ -26,3 +26,24 @@ int solve(vector<int>& nums, int target,int n,vector<int>&dp)
     }
 };
 //Tabulation
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<unsigned int>dp(target+1,0);
+        //base case
+        dp[0]=1;
+        for(int t=1;t<=target;t++)
+        {
+            for(int i=0;i<n;i++)
+            {
+               int remaningTarget=t-nums[i];
+               if(remaningTarget>=0)
+               {
+                dp[t]=dp[t]+dp[remaningTarget];
+               }
+            } 
+        }
+        return dp[target];
+    }
+};
