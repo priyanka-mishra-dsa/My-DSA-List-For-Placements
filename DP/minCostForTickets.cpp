615bbb2155c3b69f78f70ba1d15f04d4be078386
Priyanka Mishra <prmishra3085@gmail.com>
2026-07-10 18:48:00 +0530

    Update minCostForTickets.cpp

--- DP/minCostForTickets.cpp
+++ DP/minCostForTickets.cpp
@@ -39,3 +39,33 @@ int solve(int index,vector<int>& days, vector<int>& costs,int n,vector<int>&dp)
         return ans;    
     }
 };
+//Tabulation
+class Solution {
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        int n=days.size();
+        vector<int>dp(n+1,-1);
+        dp[n]=0;
+        for(int index=n-1;index>=0;index--) 
+        {
+           //cost for buy 1 day pass for current index
+           int cost_1=costs[0]+dp[index+1];
+           //cost for buy 7 day pass for current index
+           int j=index;
+           int max_day=days[j]+7;
+           while(j<n && days[j]<max_day)
+           j++;
+           int cost_7=costs[1]+dp[j];
+           //cost for buy 30 day pass for current index
+           int k=index;
+           max_day=days[k]+30;
+           while(k<n && days[k]<max_day)
+           k++;
+           int cost_30=costs[2]+dp[k];
+           int minCost=min({cost_1,cost_7,cost_30});
+           //store
+           dp[index]=minCost;
+        }
+        return dp[0];
+    }
+};
