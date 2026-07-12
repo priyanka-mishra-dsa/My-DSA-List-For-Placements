c7ca399e0eaf76aad8753c954ed83179855f406d
Priyanka Mishra <prmishra3085@gmail.com>
2026-07-12 14:41:05 +0530

    Update uniquePaths.cpp

--- DP/uniquePaths.cpp
+++ DP/uniquePaths.cpp
@@ -28,24 +28,6 @@ int solve(int i,int j,int m,int n,vector<vector<int>>&dp)
 //Tabulation
 class Solution {
 public:
-int solve(int i,int j,int m,int n,vector<vector<int>>&dp)
-{
-    //base case
-    if(i==m-1 && j==n-1)
-    return 1;
-    if(i<0 || i>=m || j<0 || j>=n)//out of bound cond
-    return 0;
-    //extra
-    if(dp[i][j]!=-1)
-    return dp[i][j];
-    //2 ways to move
-    int right=solve(i,j+1,m,n,dp);
-    int down=solve(i+1,j,m,n,dp);
-    int count=right+down;
-    //store before return
-    dp[i][j]=count;
-    return count;
-}
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m,vector<int>(n,0));
         for(int i=m-1;i>=0;i--)
