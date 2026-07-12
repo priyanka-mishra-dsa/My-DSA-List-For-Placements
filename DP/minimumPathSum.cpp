903ce97e3f9ff6fb78777536cba90090a029783f
Priyanka Mishra <prmishra3085@gmail.com>
2026-07-12 21:42:27 +0530

    Update minimumPathSum.cpp

--- DP/minimumPathSum.cpp
+++ DP/minimumPathSum.cpp
@@ -39,3 +39,45 @@ int solve(int i,int j,int row,int col,vector<vector<int>>& grid,vector<vector<in
         return ans;   
     }
 };
+//Tabulation
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int row=grid.size();
+        int col=grid[0].size();
+        int MinPathSum=0;
+        vector<vector<int>>dp(row,vector<int>(col,0));
+        for(int i=row-1;i>=0;i--)
+        {
+            for(int j=col-1;j>=0;j--)
+            {
+                //handle base case
+                if(i==row-1 && j==col-1)
+                {
+                    dp[i][j]=grid[i][j];
+                }
+                //
+                if(i==row-1)
+                {
+                    //only go in right
+                    if(j+1<col)
+                    dp[i][j]=grid[i][j]+dp[i][j+1];
+                }
+                else if(j==col-1)
+                {
+                    //only go down
+                    if(i+1<row)
+                    dp[i][j]=grid[i][j]+dp[i+1][j];
+                }
+                else
+                {
+                    // i can go on right or down both
+                    MinPathSum=grid[i][j]+min(dp[i+1][j],dp[i][j+1]);
+                    //store the value
+                    dp[i][j]=MinPathSum;
+                }
+            }
+        }
+        return dp[0][0];
+    }
+};
