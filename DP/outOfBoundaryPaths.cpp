043458006985063468d74a2b8bdaa3b2566f7527
Priyanka Mishra <prmishra3085@gmail.com>
2026-07-17 13:36:06 +0530

    Update outOfBoundaryPaths.cpp

--- DP/outOfBoundaryPaths.cpp
+++ DP/outOfBoundaryPaths.cpp
@@ -29,3 +29,48 @@ int solve(int m,int n,int maxMove,int row,int col,vector<vector<vector<int>>>&dp
         return ans;    
     }
 };
+//Tabulation
+class Solution {
+public:
+static const int MOD=1e9+7;
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        //3 states are changing everytime
+        //row,col,maxMoves
+        //use 3d dp
+        vector<vector<vector<int>>>dp(m,vector<vector<int>>(n,vector<int>(maxMove+1,0)));
+        //base case if(maxMove==0) return 0->already intialized main jab vector 0 se fill kiya
+        for(int i=1;i<=maxMove;i++)
+        {
+            for(int row=0;row<m;row++)
+            {
+                for(int col=0;col<n;col++)
+                {
+                    long long top,down,left,right;
+                    //handle for top
+                    if(row==0)
+                    top=1;
+                    else
+                    top=dp[row-1][col][i-1];
+                    //handle for down
+                    if(row==m-1)
+                    down=1;
+                    else
+                    down=dp[row+1][col][i-1];
+                    //handle left
+                    if(col==0)
+                    left=1;
+                    else
+                    left=dp[row][col-1][i-1];
+                    //handle right
+                    if(col==n-1)
+                    right=1;
+                    else
+                    right=dp[row][col+1][i-1];
+                    int totalPaths=(top+down+left+right)%MOD;
+                    dp[row][col][i]=totalPaths;
+                }
+            }
+        }
+        return dp[startRow][startColumn][maxMove];
+    }
+};
