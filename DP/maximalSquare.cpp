327cacd3ebdef90ecc2d5da858640c6ec56d4783
Priyanka Mishra <prmishra3085@gmail.com>
2026-07-19 12:47:08 +0530

    Update maximalSquare.cpp

---
+++ DP/maximalSquare.cpp
@@ -0,0 +1,43 @@
+//recursion+memorization
+class Solution {
+public:
+int m,n;
+int solve(int row,int col,vector<vector<char>>& grid,vector<vector<int>>&dp)
+{
+    //handle out of bound case
+    if(row<0 || row>=m || col<0 || col>=n)
+    return 0;
+    //handle base case
+    if(grid[row][col]=='0')
+    return 0;
+    //
+    if(dp[row][col]!=-1)
+    return dp[row][col];
+    //
+    int right=solve(row,col+1,grid,dp);
+    int down=solve(row+1,col,grid,dp);
+    int diagonal=solve(row+1,col+1,grid,dp);
+    int total=1+min({right,down,diagonal});
+    dp[row][col]=total;
+    return total;
+}
+    int maximalSquare(vector<vector<char>>& grid) {
+        m=grid.size();
+        n=grid[0].size();
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        //find largest square
+        int maxSq=0,ans=0;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                ans=solve(i,j,grid,dp);
+                maxSq=max(maxSq,ans);
+            }
+        }
+        //return area
+        int area=maxSq*maxSq;
+        return area;   
+    }
+};
+//Tabulation
