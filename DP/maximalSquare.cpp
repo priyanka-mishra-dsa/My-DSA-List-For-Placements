416d0caf24cd65630ccc071cf7db0a96f98508d7
Priyanka Mishra <prmishra3085@gmail.com>
2026-07-19 13:10:32 +0530

    Update maximalSquare.cpp

--- DP/maximalSquare.cpp
+++ DP/maximalSquare.cpp
@@ -41,3 +41,35 @@ int solve(int row,int col,vector<vector<char>>& grid,vector<vector<int>>&dp)
     }
 };
 //Tabulation
+class Solution {
+public:
+int m,n;
+    int maximalSquare(vector<vector<char>>& grid) {
+        m=grid.size();
+        n=grid[0].size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        int maxSq=0;
+        for(int row=m-1;row>=0;row--)
+        {
+            for(int col=n-1;col>=0;col--)
+            {
+               if(grid[row][col]=='0')
+               {
+                  dp[row][col]=0;
+               }
+               else if(grid[row][col]=='1')
+               {
+                  int right=dp[row][col+1];
+                  int down=dp[row+1][col];
+                  int diagonal=dp[row+1][col+1];
+                  int ans=1+min({right,down,diagonal});
+                  dp[row][col]=ans;
+                  //find largest square
+                  maxSq=max(maxSq,ans);
+               }
+            }
+        }
+        int area=maxSq*maxSq;
+        return area;     
+    }
+};
