2a0e64baef9568b5b30a03ed7ce6a88564d2c61c
Priyanka Mishra <prmishra3085@gmail.com>
2026-07-28 20:42:34 +0530

    Update cloneGraph.cpp

--- Graphs/cloneGraph.cpp
+++ Graphs/cloneGraph.cpp
@@ -1,43 +1,42 @@
 class Solution {
 public:
     Node* cloneGraph(Node* node) {
-        //give only one node of graph
-        //make clone of only that node of graph which have same value but different address
-        //for making clone of neig node ,you need to travarse on given graph neig node using bfs/dfs
-        //take a map to store visited node(jiska clone bana liya hai)
+        //edge case
         if(node==NULL)
         return NULL;
-        unordered_map<Node*,Node*>m;//original node,clone node
-        Node*cloneNode=new Node(node->val);//clone of given node
-        //after making clone mark as visited
-        m[node]=cloneNode;
+        //solve using bfs
+        //for bfs we need queue and visited arr(unordered_map)
+        unordered_map<Node*,Node*>m;
         queue<Node*>q;
+        //clone the node
+        Node*cloneNode=new Node(node->val);
+        //mark as visited
+        m[node]=cloneNode;
+        //push into queue
         q.push(node);
         while(!q.empty())
         {
-            Node*temp=q.front();
+            Node* currNode=q.front();
             q.pop();
-            //visit its neig node
-            for(int i=0;i<temp->neighbors.size();i++)
+            //clone neig node of current node
+            for(int i=0;i<currNode->neighbors.size();i++)
             {
-                Node*neig=temp->neighbors[i];
-                //if neig node is not clone(not visited)
-                if(m.count(neig)==0)//not present in the map
+                Node* neig=currNode->neighbors[i];
+                //clone neig node
+                Node* neigClone=new Node(neig->val);
+                //check visited or not
+                if(m.count(neig)==0)
                 {
-                    //clone (create node)
-                    Node*cloneNeig=new Node(neig->val);
                     //mark as visited
-                    m[neig]=cloneNeig;
-                    //push in the queue(always push original node because we apply bfs on original node)
+                    m[neig]=neigClone;
+                    //push into queue
                     q.push(neig);
                 }
-                //make connection b/w cloneNode & cloneNeig node
-                //m[temp]==cloneNode
-                m[temp]->neighbors.push_back(m[neig]);
+                 //connect clone nodes together
+                m[currNode]->neighbors.push_back(m[neig]);          
             }
         }
-        //return starting node of clone graph
-        //m[node]=cloneNode(return)
+        //return first node of cloned graph
         return m[node];
     }
 };
