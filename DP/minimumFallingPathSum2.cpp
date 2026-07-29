66674d4f12a8c07fe25cd3752bc7ce2c0f27d348
Priyanka Mishra <prmishra3085@gmail.com>
2026-07-29 17:52:21 +0530

    Update minimumFallingPathSum2.cpp

--- DP/minimumFallingPathSum2.cpp
+++ DP/minimumFallingPathSum2.cpp
@@ -42,3 +42,46 @@ int solve(int row,int col,vector<vector<int>>& grid,vector<vector<int>>&dp)
         return minSum;    
     }
 };
+//Tabulation
+class Solution {
+public:
+int n;
+    int minFallingPathSum(vector<vector<int>>& grid) {
+        n=grid.size();
+        vector<vector<int>>dp(n,vector<int>(n,INT_MAX));
+        //handle base case(for last row)
+        for(int col=0;col<n;col++)
+        {
+            dp[n-1][col]=grid[n-1][col];
+        }
+        //start iteration from second last row
+        for(int row=n-2;row>=0;row--)
+        {
+            //also have multiple options to choose element of current row
+            for(int col=0;col<n;col++)
+            {
+                //current row col value
+                int current=grid[row][col];
+                int minSum=INT_MAX,ans=INT_MAX;
+                //choose element from next row(we have multiple options)
+                for(int nextCol=0;nextCol<n;nextCol++)
+                {
+                    if(col!=nextCol)
+                    {
+                        ans=current+dp[row+1][nextCol];
+                        minSum=min(ans,minSum);
+                    }
+                }
+                dp[row][col]=minSum;
+            }
+        }
+        //start from any col of first row
+        int result=INT_MAX,minAns=INT_MAX;
+        for(int col=0;col<n;col++)
+        {
+            result=dp[0][col];
+            minAns=min(result,minAns);
+        }
+        return minAns;
+    }
+};
