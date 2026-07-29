881da7e8f939c98ad83005f910cfd7a9c6f677bc
Priyanka Mishra <prmishra3085@gmail.com>
2026-07-29 21:21:49 +0530

    Update allPathsFromSrcToTarget.cpp

--- Graphs/allPathsFromSrcToTarget.cpp
+++ Graphs/allPathsFromSrcToTarget.cpp
@@ -1,38 +1,37 @@
- //find all the paths from first node(source node) to last node(target) and store them.
-//Graph->Directed acyclic graph means no cycle is present in the graph
-//we cannot apply bfs here
-//just do dfs traversal
-//do not take visited vector because you miss the valid path ,use backtracking 
-class Solution {
+ class Solution {
 public:
-void dfs(vector<vector<int>>& graph,int source,int target,vector<int>&path,vector<vector<int>>&result)
+vector<vector<int>>result;
+vector<int>path;
+void solve(int src,int target,vector<vector<int>>& graph)
 {
-    int node=source;
+    int node=src;
     path.push_back(node);
-    //if node==target means we get the path
+    //base case
     if(node==target)
     {
+        //we get a path
         result.push_back(path);
     }
-    //if node!=target then visit all the neig
-    else
+    else if(node!=target)
     {
+        //visit neig of curr node
         for(int i=0;i<graph[node].size();i++)
         {
             int neig=graph[node][i];
-            dfs(graph,neig,target,path,result);
+            solve(neig,target,graph);
         }
     }
-    //do backtracking to find all possible path
+    //use backtracking for next path
     path.pop_back();
 }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        //no of nodes
         int n=graph.size();
-        int source=0;
+        //source
+        int src=0;
+        //target
         int target=n-1;
-        vector<vector<int>>result;
-        vector<int>path;
-        dfs(graph,source,target,path,result);
-        return result;      
+        solve(src,target,graph);
+        return result;   
     }
 };
